81713aa75603d5af7de7168c3078deb409dd0771
zori0004 <39057363+zori0004@users.noreply.github.com>
2018-08-13 13:08:25 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const homeContainer = document.querySelector(".home-content");
+let homeUrl = "../partials/articles.json";
+
+function articles() {
+    fetch(homeUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let ca of data.articles) {
+                homeContainer.innerHTML += `<hr>
+                                        <h3>${ca.title}</h3>
+                                        <div class="articles clearfix">
+                                            <figure>
+                                                <img src="${ca.img}" alt="${ca.alt}">
+                                                <em><figcaption>${ca.fig}</figcaption></em>
+                                            </figure>
+                                            <div class="about-article">
+                                                <p>${ca.text}</p>
+                                            </div>
+                                        </div>`;
+            }
+        })
+        .catch(function (error) {
+            console.log(error.message);
+        });
+}
+
+window.onload = articles;
